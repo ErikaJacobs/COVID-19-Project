d9cefa134da340df675e52a1cdfa1f1053f1b994
erikajacobs <ErikaAshley3@gmail.com>
2020-12-07 13:10:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -136,6 +136,7 @@ def incrdecr(now, then):
 
 external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div([
     
