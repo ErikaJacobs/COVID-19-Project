7fbd8fcff0632b07c0af104b34e44b57a8d41e46
erikajacobs <ErikaAshley3@gmail.com>
2020-12-29 21:34:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -349,7 +349,7 @@ def update_fig(n_clicks, value):
                   barmode = 'stack',
                   paper_bgcolor = 'rgba(0,0,0,0)',
 				  plot_bgcolor = 'rgba(0,0,0,0)',
-                  colorway = ['#9a3033', '#d66c47', '#e0c553'],
+                  colorway = ['#9a3033', '#e0c553', '#d66c47'],
                   textfont = dict(color="White"),
                   colorscale = 'RdBu')
     fig = dict(data = data, layout = layout)
